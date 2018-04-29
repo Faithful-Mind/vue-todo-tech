f5a36f317883708cfcead0dc832cddab730e6dfb
Faithful-Mind <lpyy15@gmail.com>
2018-04-29 19:44:09 +0800

    webpack-dev-server configs & usages

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
-module.exports = {
+const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
+  target: 'web',
   entry: path.join(__dirname, 'src/index.js'),
   output: {
     filename: 'bundle.js',
@@ -40,5 +45,33 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
+  plugins: [
+    // similar to C-lang's #define keyword. Hence string need to be in quotes
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: isDev ? '"development"' : '"production"' // Defines here. So you can refer to it in this project.
+      }
+    }),
+    new HTMLPlugin()
+  ]
 }
+
+if (isDev) {
+  config.devtool = '#cheap-module-eval-source-map' // map the compiled code to source code in browser's dev tool
+  config.devServer = {
+    port: 8000,
+    host: '0.0.0.0',
+    overlay: {
+      errors: true, // show errors on web page
+    },
+    hot: true, // only refresh changed components instead of whole page. Need to pair with corresponding plugins.
+    // histoyFallback: {} // used to map unknown path to index.html
+  },
+  config.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin
+  )
+}
+
+module.exports = config
